3e6e09391c40efe02b5fe8bd2acf79918661d820
Joshua Kelso <joshuas@kainos.com>
2025-03-25 15:04:28 +0000

    add task_manager_app for getting and creating tasks

---
+++ src/task_manager_app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, jsonify
+import sqlite3
+
+app = Flask(__name__)
+
+def init_db():
+    conn = sqlite3.connect('tasks.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, name TEXT, done BOOLEAN)''')
+    conn.commit()
+    conn.close()
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    task = request.json
+    conn = sqlite3.connect('tasks.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO tasks (name, done) VALUES (?, ?)", (task['name'], task['done']))
+    conn.commit()
+    conn.close()
+    return jsonify(task), 201
+
+@app.route('/get-tasks', methods=['GET'])
+def get_tasks():
+    conn = sqlite3.connect('tasks.db')
+    c = conn.cursor()
+    res = c.execute("SELECT name, done FROM tasks")
+    tasks = res.fetchall()
+    conn.close()
+    return jsonify(tasks), 201
+
+if __name__ == '__main__':
+    init_db()
+    app.run(debug=True)
